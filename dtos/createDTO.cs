5504694ecdb8aa439ce898e6c5bc00cf3f6ad014
unknown <brunoaguiarprofissional@gmail.com>
2026-06-27 11:47:18 -0300

    refactor: adjust required DTO, validation

--- dtos/createDTO.cs
+++ dtos/createDTO.cs
@@ -4,9 +4,11 @@ namespace TaskManagerAPI.DTOs
 {
     public class CreateTarefaDto
     {
-        [Required]
+        [Required(ErrorMessage = "É obrigatório conter um título para a tarefa.")]
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
 
+        [MaxLength(200)]
         public string Description { get; set; }
 
         public string Status { get; set; }
